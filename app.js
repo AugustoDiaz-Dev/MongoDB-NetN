68a92e33b117535db16fd172587e9a0361e69a1f
Augusto <augustodiaz_1@hotmail.com>
2022-06-21 00:34:20 +1000

    MongoClient

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const PORT = 3000;
+const { connectToDB, getDB } = require('./db');
 
 // Initialize app & middleware
 const app = express();
+
+// DB Connection
+let db;
+connectToDB((err) => {
+    if (!err) {
+        app.listen(PORT, () => {
+            console.log("App listening on port: ${PORT}")
+        });
+        db = getDB();
+    }
+})
 
 // Routes
 app.get('/books', (req, res) => {
     res.json({ mssg: "Welcome to the API" })
 })
 
-app.listen(PORT, () => {
-    console.log("App listening on port: ${PORT}")
-});
---
+++ db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+let dbConnection;
+
+module.exports = {
+    connectToDB: (cb) => {
+        MongoClient.connect('mongodb://localhost:27017/bookstore')
+            .then((client) => {
+                dbConnection = client.db();
+                return cb();
+            })
+            .catch(err => {
+                console.log(err);
+                return cb(err);
+            })
+    },
+    getDB: () => dbConnection
+}
