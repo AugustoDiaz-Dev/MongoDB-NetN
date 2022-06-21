18735a0f14d2709fe4b2a885c1050cf92b586df6
Augusto <augustodiaz_1@hotmail.com>
2022-06-21 14:23:35 +1000

    Delete request

--- app.js
+++ app.js
@@ -65,3 +65,20 @@ app.post('/books', (req, res) => {
             res.status(500).json({ error: 'Could not create a new document' });
         })
 })
+
+app.delete('/books/:id', (req, res) => {
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('books')
+            .deleteOne({ _id: ObjectId(req.params.id) })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not delete the document' })
+            })
+
+    } else {
+        res.status(500).json({ error: 'Not valid document id' })
+    }
+})
