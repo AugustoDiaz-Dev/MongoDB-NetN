0a14f9bc12d84add87cb850f012c4580b8a861d4
Augusto <augustodiaz_1@hotmail.com>
2022-06-22 00:57:03 +1000

    Mongo Atlas

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require('mongodb');
 let dbConnection;
+let uri = 'mongodb+srv://pepebanana:4FSmxLvt8PDUPfp@cluster0.5kwcyn6.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     connectToDB: (cb) => {
-        MongoClient.connect('mongodb://localhost:27017/bookstore')
+        MongoClient.connect(uri)
             .then((client) => {
                 dbConnection = client.db();
                 return cb();
